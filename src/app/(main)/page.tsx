443c8a9cc87fb49bbb469cd9566f08f943fed754
Ag-Web-development <systemsdepartment5@gmail.com>
2025-07-26 11:01:01 +0200

    upload project in next and deployt

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-red-500 p-3 border border-red-300 rounded-md">
+        Hallo Full Ecommerce projects
+      </div>
+    </div>
+  );
+}
